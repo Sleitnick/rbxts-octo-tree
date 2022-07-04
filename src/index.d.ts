81c4dbed116393a4528c564c83044d18614f5af1
osyrisrblx <osyrisrblx@gmail.com>
2022-07-04 14:31:27 -0400

    Rework types to better reflect Lua source

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,25 @@
-/**
- * An octree node represents a point in 3D space within an octree,
- * along with an arbitrary object attached to it.
- *
- * To change the position of a node, go through the
- * `octree.ChangeNodePosition()` method.
- */
-export interface Node<T> {
-	readonly Position: Vector3;
-	readonly Object: T;
+declare namespace Octree {
+	/**
+	 * An octree node represents a point in 3D space within an octree,
+	 * along with an arbitrary object attached to it.
+	 *
+	 * To change the position of a node, go through the
+	 * `octree.ChangeNodePosition()` method.
+	 */
+	interface Node<T> {
+		readonly Position: Vector3;
+		readonly Object: T;
+	}
+
+	interface Constructor {
+		/**
+		 * Constructs a new octree.
+		 *
+		 * The `topRegionSize` defaults to `512`, which should be fine for
+		 * most applications, but can be set to a different number if desired.
+		 */
+		new <T>(topRegionSize?: number): Octree<T>;
+	}
 }
 
 interface Octree<T> {
@@ -16,7 +28,7 @@ interface Octree<T> {
 	 * @param position Position of the node.
 	 * @param object Arbitrary object to hold within the node.
 	 */
-	CreateNode(position: Vector3, object: T): Node<T>;
+	CreateNode(position: Vector3, object: T): Octree.Node<T>;
 
 	/**
 	 * Removes the given node from the octree.
@@ -24,7 +36,7 @@ interface Octree<T> {
 	 * Removing a node does _not_ modify the held object.
 	 * @param node The node to remove.
 	 */
-	RemoveNode(node: Node<T>): void;
+	RemoveNode(node: Octree.Node<T>): void;
 
 	/**
 	 * Changes the position of the node. This will write the `.Position` property
@@ -36,7 +48,7 @@ interface Octree<T> {
 	 * @param node The node to change.
 	 * @param position The new position for the node.
 	 */
-	ChangeNodePosition(node: Node<T>, position: Vector3): void;
+	ChangeNodePosition(node: Octree.Node<T>, position: Vector3): void;
 
 	/**
 	 * Get a list of all nodes.
@@ -44,7 +56,7 @@ interface Octree<T> {
 	 * This can be an expensive operation, as all regions and subregions
 	 * must be traversed to find all the nodes.
 	 */
-	GetAllNodes(): Array<Node<T>>;
+	GetAllNodes(): Array<Octree.Node<T>>;
 
 	/**
 	 * Iterate over each node.
@@ -55,7 +67,7 @@ interface Octree<T> {
 	 * }
 	 * ```
 	 */
-	ForEachNode(): IterableFunction<Node<T>>;
+	ForEachNode(): IterableFunction<Octree.Node<T>>;
 
 	/**
 	 * Counts all of the nodes in the octree.
@@ -80,14 +92,14 @@ interface Octree<T> {
 	 * found, then the returned value will be `undefined`.
 	 * @param object The object to find.
 	 */
-	FindFirstNode(object: T): Node<T> | undefined;
+	FindFirstNode(object: T): Octree.Node<T> | undefined;
 
 	/**
 	 * Search for all nodes within the radius around the given position.
 	 * @param position The central position to look around.
 	 * @param radius The radius around the position.
 	 */
-	RadiusSearch(position: Vector3, radius: number): Array<Node<T>>;
+	RadiusSearch(position: Vector3, radius: number): Array<Octree.Node<T>>;
 
 	/**
 	 * Returns a sorted list of nodes from closest to farthest. This is
@@ -97,21 +109,12 @@ interface Octree<T> {
 	 * @param radius The radius around the position.
 	 * @param maxNodes Max returned nodes (No limit if not provided).
 	 */
-	GetNearest(position: Vector3, radius: number, maxNodes?: number): Array<Node<T>>;
-}
-
-interface OctreeConstructor {
-	/**
-	 * Constructs a new octree.
-	 *
-	 * The `topRegionSize` defaults to `512`, which should be fine for
-	 * most applications, but can be set to a different number if desired.
-	 */
-	new <T>(topRegionSize?: number): Octree<T>;
+	GetNearest(position: Vector3, radius: number, maxNodes?: number): Array<Octree.Node<T>>;
 }
 
 /**
  * Octree implementation for Roblox game development.
  */
-declare const Octree: OctreeConstructor;
-export { Octree };
+declare const Octree: Octree.Constructor;
+
+export = Octree;
